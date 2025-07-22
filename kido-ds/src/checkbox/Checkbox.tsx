71e032e7deed0051b9b6919a870a65aaa4bd5470
tidy-dev-team <151361811+tidy-dev-team@users.noreply.github.com>
2025-07-22 12:33:31 +0300

    feat(ui): Update component gap to gap-1

--- kido-ds/src/checkbox/Checkbox.tsx
+++ kido-ds/src/checkbox/Checkbox.tsx
@@ -71,7 +71,7 @@ export const Checkbox: React.FC<CheckboxProps> = ({
   };
 
   return (
-    <label className="flex row gap-12 cursor-pointer text-base">
+    <label className="flex row gap-1 cursor-pointer text-base">
       <input
         type="checkbox"
         ref={setCheckboxRef}
