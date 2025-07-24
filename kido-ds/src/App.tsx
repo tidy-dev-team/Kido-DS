4a2acd82289d13886bbd45dfc22ae67e8d7abb01
Adir <slutzkis@gmail.com>
2025-07-24 16:55:55 +0300

    Update App.tsx

--- kido-ds/src/App.tsx
+++ kido-ds/src/App.tsx
@@ -9,6 +9,8 @@ import { Avatar } from './avatar/Avatar'
 function App() {
   const [checkboxType1, setCheckboxType1] = useState<CheckboxType>(CheckboxType.unchecked);
   const [checkboxType2, setCheckboxType2] = useState<CheckboxType>(CheckboxType.checked);
+  const [checkboxType3, setCheckboxType3] = useState<CheckboxType>(CheckboxType.indeterminate);
+  const [checkboxType4, setCheckboxType4] = useState<CheckboxType>(CheckboxType.checked);
 
   const handleCheckboxChange1 = (newState: CheckboxType) => {
     console.log("Checkbox 1 changed to:", newState);
@@ -20,21 +22,19 @@ function App() {
     setCheckboxType2(newState);
   };
 
+  const handleCheckboxChange3 = (newState: CheckboxType) => {
+    console.log("Checkbox 3 changed to:", newState);
+    setCheckboxType3(newState);
+  };
+
+  const handleCheckboxChange4 = (newState: CheckboxType) => {
+    console.log("Checkbox 4 changed to:", newState);
+    setCheckboxType4(newState);
+  };
+
   return (
     <>
-      <div className='flex gap-4 items-center'>
-        <Avatar
-          type="initials"
-          initials="AA"
-          shape="circle"
-          size="24"
-        />
-        <Avatar
-          type="initials"
-          initials="AA"
-          shape="circle"
-          size="32"
-        />
+      <div className='flex gap-5 flex-wrap items-center'>
         <Avatar
           type="initials"
           initials="AA"
@@ -44,441 +44,25 @@ function App() {
         <Avatar
           type="initials"
           initials="AA"
-          shape="circle"
+          shape="rounded"
           size="52"
-        />
-        <Avatar
-          type="initials"
-          initials="AA"
-          shape="circle"
-          size="64"
         />
 
         <Avatar
           type="picture"
           imageUrl="https://i.pravatar.cc/300"
           shape="circle"
-          size="24"
-        />
-        <Avatar
-          type="picture"
-          imageUrl="https://i.pravatar.cc/300"
-          shape="circle"
-          size="32"
-        />
-        <Avatar
-          type="picture"
-          imageUrl="https://i.pravatar.cc/300"
-          shape="circle"
-          size="40"
-        />
-        <Avatar
-          type="picture"
-          imageUrl="https://i.pravatar.cc/300"
-          shape="circle"
-          size="52"
-        />
-        <Avatar
-          type="picture"
-          imageUrl="https://i.pravatar.cc/300"
-          shape="circle"
-          size="64"
-        />
-      </div>
-      <br />
-      <div className='flex gap-4 items-center'>
-        <Avatar
-          type="initials"
-          initials="AA"
-          shape="rounded"
-          size="24"
-        />
-        <Avatar
-          type="initials"
-          initials="AA"
-          shape="rounded"
-          size="32"
-        />
-        <Avatar
-          type="initials"
-          initials="AA"
-          shape="rounded"
-          size="40"
-        />
-        <Avatar
-          type="initials"
-          initials="AA"
-          shape="rounded"
-          size="52"
-        />
-        <Avatar
-          type="initials"
-          initials="AA"
-          shape="rounded"
-          size="64"
-        />
-
-        <Avatar
-          type="picture"
-          imageUrl="https://i.pravatar.cc/300"
-          shape="rounded"
-          size="24"
-        />
-        <Avatar
-          type="picture"
-          imageUrl="https://i.pravatar.cc/300"
-          shape="rounded"
-          size="32"
-        />
-        <Avatar
-          type="picture"
-          imageUrl="https://i.pravatar.cc/300"
-          shape="rounded"
-          size="40"
-        />
-        <Avatar
-          type="picture"
-          imageUrl="https://i.pravatar.cc/300"
-          shape="rounded"
-          size="52"
-        />
-        <Avatar
-          type="picture"
-          imageUrl="https://i.pravatar.cc/300"
-          shape="rounded"
-          size="64"
-        />
-      </div>
-      <br />
-      <div className='flex gap-4 items-center'>
-        <Avatar
-          type="initials"
-          initials="AA"
-          shape="square"
-          size="24"
-        />
-        <Avatar
-          type="initials"
-          initials="AA"
-          shape="square"
-          size="32"
-        />
-        <Avatar
-          type="initials"
-          initials="AA"
-          shape="square"
-          size="40"
-        />
-        <Avatar
-          type="initials"
-          initials="AA"
-          shape="square"
-          size="52"
-        />
-        <Avatar
-          type="initials"
-          initials="AA"
-          shape="square"
-          size="64"
-        />
-
-        <Avatar
-          type="picture"
-          imageUrl="https://i.pravatar.cc/300"
-          shape="square"
-          size="24"
-        />
-        <Avatar
-          type="picture"
-          imageUrl="https://i.pravatar.cc/300"
-          shape="square"
-          size="32"
-        />
-        <Avatar
-          type="picture"
-          imageUrl="https://i.pravatar.cc/300"
-          shape="square"
-          size="40"
-        />
-        <Avatar
-          type="picture"
-          imageUrl="https://i.pravatar.cc/300"
-          shape="square"
           size="52"
         />
         <Avatar
           type="picture"
           imageUrl="https://i.pravatar.cc/300"
           shape="square"
-          size="64"
-        />
-      </div>
-      <br />
-      <div className='flex gap-4 items-center'>
-        <Avatar
-          type="initials"
-          initials="AA"
-          shape="circle"
-          size="24"
-          upperBadge
-          lowerBadge
-        />
-        <Avatar
-          type="initials"
-          initials="AA"
-          shape="circle"
-          size="32"
-          upperBadge
-          lowerBadge
-        />
-        <Avatar
-          type="initials"
-          initials="AA"
-          shape="circle"
-          size="40"
-          upperBadge
-          lowerBadge
-        />
-        <Avatar
-          type="initials"
-          initials="AA"
-          shape="circle"
           size="52"
-          upperBadge
-          lowerBadge
-        />
-        <Avatar
-          type="initials"
-          initials="AA"
-          shape="circle"
-          size="64"
-          upperBadge
           lowerBadge
         />
 
-        <Avatar
-          type="picture"
-          imageUrl="https://i.pravatar.cc/300"
-          shape="circle"
-          size="24"
-          upperBadge
-          lowerBadge
-        />
-        <Avatar
-          type="picture"
-          imageUrl="https://i.pravatar.cc/300"
-          shape="circle"
-          size="32"
-          upperBadge
-          lowerBadge
-        />
-        <Avatar
-          type="picture"
-          imageUrl="https://i.pravatar.cc/300"
-          shape="circle"
-          size="40"
-          upperBadge
-          lowerBadge
-        />
-        <Avatar
-          type="picture"
-          imageUrl="https://i.pravatar.cc/300"
-          shape="circle"
-          size="52"
-          upperBadge
-          lowerBadge
-        />
-        <Avatar
-          type="picture"
-          imageUrl="https://i.pravatar.cc/300"
-          shape="circle"
-          size="64"
-          upperBadge
-          lowerBadge
-        />
-      </div>
-      <br />
-      <div className='flex gap-4 items-center'>
-        <Avatar
-          type="initials"
-          initials="AA"
-          shape="rounded"
-          size="24"
-          upperBadge
-          lowerBadge
-        />
-        <Avatar
-          type="initials"
-          initials="AA"
-          shape="rounded"
-          size="32"
-          upperBadge
-          lowerBadge
-        />
-        <Avatar
-          type="initials"
-          initials="AA"
-          shape="rounded"
-          size="40"
-          upperBadge
-          lowerBadge
-        />
-        <Avatar
-          type="initials"
-          initials="AA"
-          shape="rounded"
-          size="52"
-          upperBadge
-          lowerBadge
-        />
-        <Avatar
-          type="initials"
-          initials="AA"
-          shape="rounded"
-          size="64"
-          upperBadge
-          lowerBadge
-        />
 
-        <Avatar
-          type="picture"
-          imageUrl="https://i.pravatar.cc/300"
-          shape="rounded"
-          size="24"
-          upperBadge
-          lowerBadge
-        />
-        <Avatar
-          type="picture"
-          imageUrl="https://i.pravatar.cc/300"
-          shape="rounded"
-          size="32"
-          upperBadge
-          lowerBadge
-        />
-        <Avatar
-          type="picture"
-          imageUrl="https://i.pravatar.cc/300"
-          shape="rounded"
-          size="40"
-          upperBadge
-          lowerBadge
-        />
-        <Avatar
-          type="picture"
-          imageUrl="https://i.pravatar.cc/300"
-          shape="rounded"
-          size="52"
-          upperBadge
-          lowerBadge
-        />
-        <Avatar
-          type="picture"
-          imageUrl="https://i.pravatar.cc/300"
-          shape="rounded"
-          size="64"
-          upperBadge
-          lowerBadge
-        />
-      </div>
-      <br />
-      <div className='flex gap-4 items-center'>
-        <Avatar
-          type="initials"
-          initials="AA"
-          shape="square"
-          size="24"
-          upperBadge
-          lowerBadge
-        />
-        <Avatar
-          type="initials"
-          initials="AA"
-          shape="square"
-          size="32"
-          upperBadge
-          lowerBadge
-        />
-        <Avatar
-          type="initials"
-          initials="AA"
-          shape="square"
-          size="40"
-          upperBadge
-          lowerBadge
-        />
-        <Avatar
-          type="initials"
-          initials="AA"
-          shape="square"
-          size="52"
-          upperBadge
-          lowerBadge
-        />
-        <Avatar
-          type="initials"
-          initials="AA"
-          shape="square"
-          size="64"
-          upperBadge
-          lowerBadge
-        />
-
-        <Avatar
-          type="picture"
-          imageUrl="https://i.pravatar.cc/300"
-          shape="square"
-          size="24"
-          upperBadge
-          lowerBadge
-        />
-        <Avatar
-          type="picture"
-          imageUrl="https://i.pravatar.cc/300"
-          shape="square"
-          size="32"
-          upperBadge
-          lowerBadge
-        />
-        <Avatar
-          type="picture"
-          imageUrl="https://i.pravatar.cc/300"
-          shape="square"
-          size="40"
-          upperBadge
-          lowerBadge
-        />
-        <Avatar
-          type="picture"
-          imageUrl="https://i.pravatar.cc/300"
-          shape="square"
-          size="52"
-          upperBadge
-          lowerBadge
-        />
-        <Avatar
-          type="picture"
-          imageUrl="https://i.pravatar.cc/300"
-          shape="square"
-          size="64"
-          upperBadge
-          lowerBadge
-        />
-      </div>
-      <br /><br />
-      <div className='flex gap-2 items-center'>
-        <Button size="l" variant="contained" iconL={<ReactLogo />} disabled>
-          Large
-        </Button>
-        <Button size="m" variant="contained" iconL={<ReactLogo />}>
-          Medium
-        </Button>
-        <Button size="s" variant="contained" iconL={<ReactLogo />}>
-          Small
-        </Button>
-      </div>
-      <br />
-      <div className='flex gap-2 items-center'>
         <Button size="l" variant="contained">
           OK
         </Button>
@@ -488,10 +72,8 @@ function App() {
         <Button size="s" variant="contained">
           OK
         </Button>
-      </div>
-      <br /><br />
-      <div className='flex gap-2 items-center'>
-        <Button size="l" variant="outlined" iconL={<ReactLogo />} disabled>
+
+        <Button size="l" variant="outlined" iconL={<ReactLogo />}>
           Large
         </Button>
         <Button size="m" variant="outlined" iconL={<ReactLogo />}>
@@ -500,44 +82,46 @@ function App() {
         <Button size="s" variant="outlined" iconL={<ReactLogo />}>
           Small
         </Button>
-      </div>
-      <br /><br />
-      <div className='flex gap-2 items-center'>
-        <Button size="l" variant="ghost" iconL={<ReactLogo />} disabled>
-          Large
-        </Button>
+
+
         <Button size="m" variant="ghost" iconL={<ReactLogo />}>
           Medium
         </Button>
-        <Button size="s" variant="ghost" iconL={<ReactLogo />}>
-          Small
-        </Button>
+
+        <Checkbox
+          type={checkboxType1}
+          onChange={handleCheckboxChange1}
+          showLabel={true}
+          labelText="Enable Feature"
+          showCount={true}
+          count={"2"}
+          showDescription={true}
+          description="This toggles the feature on or off."
+          showIcon={true}
+          icon={<ReactLogo />}
+        />
+        <Checkbox
+          type={checkboxType2}
+          onChange={handleCheckboxChange2}
+          showLabel={true}
+          labelText="Label example"
+          showCount={false}
+          showDescription={true}
+          description="Bla bla bla bla bla bla bla bla."
+          showIcon={true}
+          icon={<ReactLogo />}
+        />
+        <Checkbox
+          type={checkboxType3}
+          onChange={handleCheckboxChange3}
+          showLabel={true}
+          labelText="Label example"
+        />
+        <Checkbox
+          type={checkboxType4}
+          onChange={handleCheckboxChange4}
+        />
       </div>
-      <br /><br />
-      <Checkbox
-        type={checkboxType1}
-        onChange={handleCheckboxChange1}
-        showLabel={true}
-        labelText="Enable Feature"
-        showCount={true}
-        count={"2"}
-        showDescription={true}
-        description="This toggles the feature on or off."
-        showIcon={true}
-        icon={<ReactLogo />}
-      />
-      <br /><br />
-      <Checkbox
-        type={checkboxType2}
-        onChange={handleCheckboxChange2}
-        showLabel={true}
-        labelText="Label example"
-        showCount={false}
-        showDescription={true}
-        description="Bla bla bla bla bla bla bla bla."
-        showIcon={true}
-        icon={<ReactLogo />}
-      />
     </>
   );
 }
