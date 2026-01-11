586acd430e086d078d5f5e3c0a35ff6d06fde208
Adir <slutzkis@gmail.com>
2026-01-11 15:54:15 +0200

    Update Button.stories.ts

--- kido-ds/src/stories/Button.stories.ts
+++ kido-ds/src/stories/Button.stories.ts
@@ -1,7 +1,27 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import React from 'react';
 import { Button } from '../button/Button';
-import { IconPlus, IconArrowRight, IconDownload } from '@tabler/icons-react';
+import { 
+  IconPlus, 
+  IconArrowRight, 
+  IconArrowLeft,
+  IconDownload, 
+  IconTrash, 
+  IconCheck, 
+  IconX 
+} from '@tabler/icons-react';
+
+// Icon mapping for dropdown
+const iconMap = {
+  none: undefined,
+  plus: React.createElement(IconPlus),
+  arrow: React.createElement(IconArrowRight),
+  backarrow: React.createElement(IconArrowLeft),
+  download: React.createElement(IconDownload),
+  trash: React.createElement(IconTrash),
+  check: React.createElement(IconCheck),
+  x: React.createElement(IconX),
+};
 
 const meta: Meta<typeof Button> = {
   title: 'Components/Button',
@@ -26,19 +46,43 @@ const meta: Meta<typeof Button> = {
       control: 'text',
       description: 'Button text content',
     },
-    iconL: {
-      control: false,
-      description: 'Left icon',
-    },
-    iconR: {
-      control: false,
-      description: 'Right icon',
-    },
   },
 };
 
 export default meta;
 type Story = StoryObj<typeof Button>;
+
+// Playground with icon dropdowns
+export const Playground: Story = {
+  argTypes: {
+    iconL: {
+      control: 'select',
+      options: Object.keys(iconMap),
+      description: 'Select left icon',
+    },
+    iconR: {
+      control: 'select',
+      options: Object.keys(iconMap),
+      description: 'Select right icon',
+    },
+  } as any,
+  args: {
+    children: 'Button',
+    size: 'm',
+    variant: 'contained',
+    disabled: false,
+    iconL: 'none',
+    iconR: 'none',
+  } as any,
+  render: (args: any) => {
+    const { iconL, iconR, ...buttonProps } = args;
+    return React.createElement(Button, {
+      ...buttonProps,
+      iconL: iconMap[iconL as keyof typeof iconMap],
+      iconR: iconMap[iconR as keyof typeof iconMap],
+    });
+  },
+};
 
 export const Default: Story = {
   args: {
